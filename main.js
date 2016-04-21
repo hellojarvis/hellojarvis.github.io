9bdd9d3516d50d0cc002454759fc2003c6db045f
Lawrence Wu <lawrence@lawrencewu.me>
2016-04-20 22:24:58 -0700

    add serving text to jarvis homepage

---
+++ main.js
@@ -0,0 +1,20 @@
+requirejs.config({
+    paths: {
+        superagent: 'https://cdnjs.cloudflare.com/ajax/libs/superagent/1.2.0/superagent.min'
+    }
+});
+
+requirejs(["superagent"], function(request) {
+  setInterval(function() {
+    setServingText(request);
+  }, 1000);
+});
+
+function setServingText(request) {
+  request
+  .get("https://hellojarvis.herokuapp.com/metrics")
+  .end(function(err, res) {
+    var servingText = "Jarvis has served " + res.body['num_reminders'] + " reminders.";
+    document.getElementById('served').textContent = servingText;
+  });
+}
