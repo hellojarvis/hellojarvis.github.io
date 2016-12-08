abf1ee8b981c9a69af02b4166e8c58a7910fcd87
Lawrence Wu <lawrencewu@chromium.org>
2016-12-08 14:06:37 -0500

    add donate

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function setServingText(request) {
+  request
+  .get("https://hellojarvis.herokuapp.com/metrics")
+  .end(function(err, res) {
+    var servingText = "Jarvis sent " + res.body['num_reminders'] + " reminders to " + res.body['num_users'] + " users.";
+    document.getElementById('served').textContent = servingText;
+  });
+}
+
 requirejs.config({
     paths: {
         superagent: 'https://cdnjs.cloudflare.com/ajax/libs/superagent/1.2.0/superagent.min'
@@ -9,13 +18,37 @@ requirejs(["superagent"], function(request) {
   setInterval(function() {
     setServingText(request);
   }, 100000);
+
+	var handler = StripeCheckout.configure({
+		key: 'pk_test_TKp9AXtzoJkgWJN2W2RAr03p',
+		image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+		locale: 'auto',
+		token: function(token) {
+      console.log(token);
+      request
+      .post('https://hellojarvis.herokuapp.com/donate')
+      .send(token)
+      .end(function(err, res) {
+        console.log(err);
+        console.log(res);
+      });
+		}
+	});
+
+	document.getElementById('donate').addEventListener('click', function(e) {
+		// Open Checkout with further options:
+		handler.open({
+			name: 'Jarvis',
+			description: 'Donation',
+			amount: 200,
+      image: 'jarvis.jpg',
+      bitcoin: true
+		});
+		e.preventDefault();
+	});
+  window.addEventListener('popstate', function() {
+    handler.close();
+  });
 });
 
-function setServingText(request) {
-  request
-  .get("https://hellojarvis.herokuapp.com/metrics")
-  .end(function(err, res) {
-    var servingText = "Jarvis sent " + res.body['num_reminders'] + " reminders to " + res.body['num_users'] + " users.";
-    document.getElementById('served').textContent = servingText;
-  });
-}
+
